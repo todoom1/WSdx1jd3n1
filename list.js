ac9e0988cae4990644bd1d1a81eabb78abc4fe8d
todoom1 <aa13976383@outlook.com>
2026-03-30 16:39:54 +0800

    更新 list.js

--- list.js
+++ list.js
@@ -22,4 +22,21 @@ document.getElementById("c7n").innerHTML = ("C7");
 document.getElementById("cfn").innerHTML = ("超凡国际");
 document.getElementById("dtn").innerHTML = ("鼎天国际");
 document.getElementById("stn").innerHTML = ("胜天国际");
-document.getElementById("dfn").innerHTML = ("巅峰国际");
+document.getElementById("dfn").innerHTML = ("巅峰
+//https://tfajn.hcjwj.com/app/register.php?site_id=2081&topId=64972&selfPlanId=50806
+jl
+https://derear.fnbpsw.com/app/register.php?site_id=1059&topId=23545&selfPlanId=6630
+xsd
+https://lncqc.jgqvji.com/app/register.php?site_id=1051&topId=4711770830905352
+top
+https://gedxs.hcjwj.com/app/register.php?site_id=1057&topId=5911768303758127
+xy
+https://149.30.163.235:32011/#/link?allwin=62tPz5C%2BlE%2BdbFNplX6Q2w%3D%3D
+ng
+https://43.249.175.11:39018/#/link?join=Grw8Lr4Z10ozC2nZEBsl4Q%3D%3D
+c7
+https://pkajim.hfjcl.com/app/register.php?site_id=1032&topId=25157
+cf
+https://tfajn.hcjwj.com/app/register.php?site_id=1063&pt=C4177E06-3BF9-B3C0-A46A-BA203436439B
+dt
+
