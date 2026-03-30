c1c6580c7562157e6f29bc03f2e9331e08a5abae
todoom1 <aa13976383@outlook.com>
2026-03-30 16:38:56 +0800

    Add files via upload

---
+++ list.js
@@ -0,0 +1,25 @@
+document.getElementById("3").innerHTML = ("2026美加墨世界杯官方合作广告商");
+document.getElementById("2").innerHTML = ("🔥十年信誉平台<br>⚽世界杯官方投注平台");
+document.getElementById("1").innerHTML="WS77.MY";
+
+document.getElementById("jl").addEventListener("click", function() {  window.open(atob("11111"), '_blank');});
+document.getElementById("xsd").addEventListener("click", function() {  window.open(atob("11111"), '_blank');});
+document.getElementById("t1").addEventListener("click", function() {  window.open(atob("11111"), '_blank');});
+document.getElementById("xy").addEventListener("click", function() {  window.open(atob("11111"), '_blank');});
+document.getElementById("ng").addEventListener("click", function() {  window.open(atob("11111"), '_blank');});
+document.getElementById("c7").addEventListener("click", function() {  window.open(atob("11111"), '_blank');});
+document.getElementById("cf").addEventListener("click", function() {  window.open(atob("11111"), '_blank');});
+document.getElementById("dt").addEventListener("click", function() {  window.open(atob("11111"), '_blank');});
+document.getElementById("st").addEventListener("click", function() {  window.open(atob("11111"), '_blank');});
+document.getElementById("df").addEventListener("click", function() {  window.open(atob("11111"), '_blank');});
+
+document.getElementById("jln").innerHTML = ("超凡国际");
+document.getElementById("xsdn").innerHTML = ("新时代");
+document.getElementById("t1n").innerHTML = ("TOP1体育");
+document.getElementById("xyn").innerHTML = ("星耀国际");
+document.getElementById("ngn").innerHTML = ("NG28");
+document.getElementById("c7n").innerHTML = ("C7");
+document.getElementById("cfn").innerHTML = ("超凡国际");
+document.getElementById("dtn").innerHTML = ("鼎天国际");
+document.getElementById("stn").innerHTML = ("胜天国际");
+document.getElementById("dfn").innerHTML = ("巅峰国际");
